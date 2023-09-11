ae96ab15a7dbb32e3adc30055efe946741b4f2c1
helio <paul61843@w101.com.tw>
2023-09-11 11:29:14 +0000

    更新 環境變數

--- deliveryEnvironment/index.py
+++ deliveryEnvironment/index.py
@@ -15,10 +15,17 @@ class DeliveryEnvironment(object):
         print(f"Target metric for optimization is {method}")
 
         # Environment Config
-        self.point_range = 5 # 節點通訊半徑範圍 (單位m)
-        self.uav_range = 10 # 無人機通訊半徑範圍 (單位m)
-        self.drift_range = 5 # 節點飄移範圍 (單位m)
-        self.max_move_distance = 300 # 無人機最大移動距離 (單位m)
+        self.point_range = 20 # 節點通訊半徑範圍 (單位m)
+        self.drift_range = 30 # 節點飄移範圍 (單位m)
+        
+        # UAV Config
+        self.uav_range = 30 # 無人機通訊半徑範圍 (單位m)
+        self.uav_speed = 5 # 無人機移動速度 (單位 m/s)
+        self.uav_energy = 100 * 1000 # 無人機電量 (單位w)
+        self.uav_energy_consumption = 100 # 無人機每秒消耗電量 (單位w)
+        self.uav_flyTime = self.uav_energy / self.uav_energy_consumption # 無人機可飛行時間 1000秒 (單位s)
+        self.max_move_distance = 5000 # 無人機最大移動距離 (單位m)
+        
 
         # 無人機探索，飄移節點最大能量消耗
         # 假設無人機只需飛行一圈，即可完整探索感測器飄移可能區域
@@ -129,7 +136,7 @@ class DeliveryEnvironment(object):
         [init_x, init_y] = init_position
         for i in self.stops:
             drift_distance = np.sqrt((self.x[i] - init_x[i]) ** 2 + (self.y[i] - init_y[i]) ** 2)
-            if (drift_distance <= self.point_range) or drift_consider:
+            if (drift_distance <= self.uav_range) or drift_consider:
                 self.uav_data = self.uav_data + self.data_amount_list[i]
                 self.data_amount_list[i] = 0
 
